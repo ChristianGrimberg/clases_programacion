4bcd7f1be8d4482a8390e0ae0259c84bd0a4e6dd
Christian Grimberg <christian.grimberg@outlook.com>
2018-08-23 19:27:50 -0300

    :+1: Busqueda de maximo y minimo.

--- Clase02/main.c
+++ Clase02/main.c
@@ -3,10 +3,36 @@
 
 int main()
 {
-    int numbers[3];
+    int number;
     int maxNumber;
     int minNumber;
+    int qtyNumbers = 10;
+    int i;
 
+    for(i=0; i<qtyNumbers; i++)
+    {
+        printf("Ingrese el numero %d: ", i+1);
+        scanf("%d", &number);
+
+        if(i==0)
+        {
+            maxNumber=number;
+            minNumber=number;
+        }
+        else
+        {
+            if(number>maxNumber)
+            {
+                maxNumber=number;
+            }
+            else if(number<minNumber)
+            {
+                minNumber=number;
+            }
+        }
+    }
+
+    printf("\nMinimo: %d\nMaximo: %d", minNumber, maxNumber);
 
     return 0;
 }
