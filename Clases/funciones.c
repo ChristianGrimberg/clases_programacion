7d17d4055c57b2a38afd76ef1e8da6981839126a
alumno <alumno@lab6pc0>
2018-08-29 22:14:44 -0300

    :+1: Se agrega la funcion de limpieza de buffer de entrada de teclado.

--- Clases/funciones.c
+++ Clases/funciones.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio_ext.h>
 #include <stdlib.h>
 #include "funciones.h"
 
@@ -11,14 +11,18 @@ int getEdad(int* pEdad)
     do
     {
         printf("Ingrese la edad: ");
-        setbuf(stdin, NULL);
+        //setbuf(stdin, NULL);
         ingreso = scanf("%d", &edadAux);
+
         if(ingreso == 1 && edadAux >= 0 && edadAux <= 199)
         {
             *pEdad = edadAux;
             retorno = 0;
             break;
         }
+        else
+            /**< Funcion extendida en stdio_ext.h para limpiar el buffer de entrada */
+            __fpurge(stdin);
     } while(ingreso != 1);
 
     return retorno;
