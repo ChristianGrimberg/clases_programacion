c495fd757c930c657a3ad3889d9f517d3c4f266d
alumno <alumno@lab6pc0>
2018-09-05 22:18:40 -0300

    :+1: Se agrega un flag para controlar la busqueda de maximo y a su vez si encuentra un maximo distinto al inicializador.

--- Clases/clases.c
+++ Clases/clases.c
@@ -5,6 +5,8 @@
 #include "clases.h"
 #define CANTIDAD_EMPLEADOS 5
 #define INICIALIZADOR -2
+#define true 1
+#define false 0
 
 int global; /**< Prueba de uso de variable global en otro archivo */
 
@@ -107,19 +109,35 @@ int calcularMaximoArray(int *pArray, int limite, int *pMaximo)
 {
     int retorno = -1;
     int maximo;
+    int flagPrimerMaximo = false;
     int i;
 
     if(pArray!=NULL && limite>0)
     {
-        retorno = 0;
         for(i=0; i<limite; i++)
         {
-            if(i==0)
-                maximo = pArray[i];
-            else if(pArray[i]>maximo)
-                maximo = pArray[i];
+            if(pArray[i]!=INICIALIZADOR)
+            {
+
+                if(flagPrimerMaximo == true && pArray[i]>maximo)
+                    maximo = pArray[i];
+                else if(flagPrimerMaximo == false)
+                {
+                    maximo = pArray[i];
+                    flagPrimerMaximo = true;
+                }
+            }
         }
-        *pMaximo = maximo;
+
+        if(flagPrimerMaximo == false)
+        {
+            retorno = -2;
+        }
+        else if(flagPrimerMaximo == true)
+        {
+            retorno = 0;
+            *pMaximo = maximo;
+        }
     }
 
     return retorno;
@@ -136,6 +154,7 @@ int initArray(int* pArray, int limite, int valor)
         for(i=0; i<limite; i++)
         {
             pArray[i] = valor;
+            //*(pArray + 1) = valor;
         }
     }
 
