ae9402fbf5503c0d2dc1f835df5eada5043dfb23
Christian Grimberg <christian.grimberg@outlook.com>
2018-09-09 20:17:50 -0300

    :+1: Se comenta las invocaciones en main y se limpia la pantalla previo a su ejecicion.

--- Clases/main.c
+++ Clases/main.c
@@ -6,13 +6,19 @@ extern int global; /**< Uso de variable global en otro source previamente declar
 
 int main()
 {
+    #ifdef __unix__
+        system("clear");
+    #else
+        system("cls");
+    #endif // __unix__
+    
     //clase02(); /**< Maximos, Minimos y Promedios. */
     //clase03(); /**< Uso de funciones y parametros por referencia. */
     //clase04(); /**< Funcion generica para obtener enteros en un rango. */
-    //clase05(); /**< Uso de array, inicializacion, busqueda de mayor. */
-    //clase06(); /**< Uso de array de enteros y ordenamiento por Bubble Sort. */ 
-    //clase06_2();
-    clase06_3();
+    //clase05(); /**< Uso de array y busqueda de mayor. */
+    //clase06(); /**< Uso de funcion publica para obtener un entero. */
+    //clase06_2();  /**< Uso de array de enteros y ordenamiento por Bubble Sort. */ 
+    clase06_3(); /**< Uso de array de flotantes y ordenamiento por Bubble Sort. */ 
 
     return 0;
 }
