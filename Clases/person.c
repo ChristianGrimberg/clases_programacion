b34a5786bf2528e0d589c980a187f42ba518dcdb
Christian Grimberg <christian.grimberg@outlook.com>
2018-09-14 14:15:25 -0300

    :wrench: Se agrega los archivos nuevos de las clases en la compilacion de vscode.

--- Clases/person.c
+++ Clases/person.c
@@ -17,7 +17,7 @@ int person_altaPersona(Persona* persona)
         if(utn_getNombre(nombreAux, NOMBRE_LIMITE, REINTENTOS, "Ingrese el nombre: ", "Error de ingreso. ") == 0
             && utn_getInt(&edadAux, REINTENTOS, 0, 200, "Ingrese la edad: ", "Error de ingreso. ") == 0
             && utn_getNombre(dniAux, DNI_LIMITE, REINTENTOS, "Ingrese el DNI: ", "Error de ingreso. ") == 0
-            && utn_getFloat(&alturaAux, REINTENTOS, 0, 500, "Ingrese su peso: ", "Error de ingreso. ") == 0)
+            && utn_getFloat(&alturaAux, REINTENTOS, 0, 3, "Ingrese su altura en metros: ", "Error de ingreso. ") == 0)
         {
             strncpy(persona->nombre, nombreAux, NOMBRE_LIMITE);
             persona->edad = edadAux;
@@ -31,5 +31,5 @@ int person_altaPersona(Persona* persona)
 
 void person_imprimirPersona(Persona* persona)
 {
-    printf("Nombre: %s\nEdad: %d\nDNI: %s\nAltura: %.2f", persona->nombre, persona->edad, persona->dni, persona->altura);
+    printf("Nombre: %s\nEdad: %d\nDNI: %s\nAltura: %.2f\n", persona->nombre, persona->edad, persona->dni, persona->altura);
 }
